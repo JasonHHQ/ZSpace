db084e32ad3f6e13e21cccc72d3c61dfb58e1b62
Jason Huang <qihuang17@126.com>
2016-11-23 18:18:54 +0800

    test.vue

---
+++ src/components/TestBody3.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+	<ul>
+		<li v-for="n in even(this.numbers)">{{ n }}</li>
+	</ul>
+	<input type="text" @keyup.enter="addnewnumber" v-model="value">
+	</div>
+</template>
+<script>
+	export default{
+		data(){ return {
+			value:'',
+		  numbers: [ 1, 2, 3, 4, 5 ]
+		}},
+		methods:{
+			even: function () {
+		    return this.numbers.filter(function (number) {
+		      return number % 2 === 0
+		    })
+		  },
+			addnewnumber(){
+				this.numbers.push(this.value)
+				this.value=''
+			}
+		}
+	}
+</script>
